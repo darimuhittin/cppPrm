3c80c58a613001519630b3309391897c2e05bf32
Muhittin Darı <darimuhittin@gmail.com>
2021-03-25 02:29:15 +0300

    init

---
+++ First/one.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main(int argc, char const *argv[])
+{
+    Sales_item book;
+    std::cin >> book;
+    std::cout << book << std::endl;
+
+    return 0;
+}
